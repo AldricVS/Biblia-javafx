3dd9c0f4aec7d9ce85d73130c6d391a0ecb3dc91
AldricVS <aldric.silvestre@outlook.fr>
2020-09-13 18:44:51 +0200

    ...

--- src/application/misc/AlertHelper.java
+++ src/application/misc/AlertHelper.java
@@ -12,7 +12,6 @@ import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.ChoiceDialog;
-import javafx.scene.control.Dialog;
 import javafx.scene.control.TextInputDialog;
 
 /**
